dce3ba1c547a1021a4be0cdd80bf007ccc2dfdcb
jcgagne <jean-christian.gagne@ne.ch>
2025-11-26 09:54:26 +0100

    support pour sessions indépendantes

--- src/main/java/ch/bdt/spike/spring/cloud/sessionapplication/config/SecurityConfig.java
+++ src/main/java/ch/bdt/spike/spring/cloud/sessionapplication/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ch.bdt.spike.spring.cloud.sessionapplication.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,11 +14,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
@@ -33,14 +38,25 @@ public class SecurityConfig {
         http.formLogin(Customizer.withDefaults())
             //.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
             //        SessionCreationPolicy.ALWAYS))
-            .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.maximumSessions(
-                    1))
+            //.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.maximumSessions(
+            //        1))
             .authorizeHttpRequests((authorizeRequests) -> authorizeRequests.requestMatchers("/")
                                                                            .hasRole("ADMIN")
                                                                            .anyRequest()
                                                                            .authenticated());
         return http.build();
     }
+
+    /**
+     * Pour avoir des sessions indépendantes, le cookie de session doit porter un nom différent.
+     * Voir aussi spring.session.data.redis.namespace.
+     */
+    //@Bean
+    //public CookieSerializer cookieSerializer() {
+    //    DefaultCookieSerializer s = new DefaultCookieSerializer();
+    //    s.setCookieName(applicationName + "_SESSION");
+    //    return s;
+    //}
 
     @Bean
     public PasswordEncoder passwordEncoder() {
